479ee0ee88a3c1957f998098a2dafed73e5ed87a
Alishah464 <a84156447@gmail.com>
2026-06-29 23:59:32 +0500

    fix: GTM script back to raw script tag in head (beforeInteractive renders in body)

--- app/layout.tsx
+++ app/layout.tsx
@@ -218,6 +218,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MFF362PW');` }} />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-8EQ10MNNGP" />
         <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-8EQ10MNNGP');` }} />
         <script
@@ -228,11 +229,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body className="bg-dark text-white antialiased noise-overlay">
-        <Script
-          id="gtm"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MFF362PW');` }}
-        />
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFF362PW" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} /></noscript>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-8EQ10MNNGP" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">{`
